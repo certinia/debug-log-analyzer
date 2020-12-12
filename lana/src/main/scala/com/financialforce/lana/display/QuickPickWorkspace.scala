7d8bc6fbc409e24296d02d8c561492749ee6b336
Kevin Jones <nawforce@gmail.com>
2020-12-12 17:11:09 +0000

    Handle no workspace by defaulting to parent of active text document

--- lana/src/main/scala/com/financialforce/lana/display/QuickPickWorkspace.scala
+++ lana/src/main/scala/com/financialforce/lana/display/QuickPickWorkspace.scala
@@ -4,6 +4,8 @@
 package com.financialforce.lana.display
 
 import com.financialforce.lana.Context
+import com.financialforce.lana.runtime.vscode.window
+import io.scalajs.nodejs.path.Path
 
 import scala.concurrent.Future
 import scala.concurrent.Future.{failed, successful}
@@ -17,10 +19,13 @@ object QuickPickWorkspace {
         new QuickPick.Item(ws.name, ws.path, "")
       }, new QuickPick.Options("Select a workspace:")) flatMap {
         case results if results.length == 1 => successful(results.head.description)
-        case _                              => failed(new Exception("No workspace selected"))
+        case _ => failed(new Exception("No workspace selected"))
       }
-    } else {
+    } else if (context.workspaces.length == 1) {
       successful(context.workspaces.head.path)
+    } else {
+      val parent = window.activeTextEditor.flatMap(te => Path.parse(te.document.uri.fsPath).dir)
+      parent.map(p => successful(p)).getOrElse(failed(new Exception("No workspace selected")))
     }
 
 }
