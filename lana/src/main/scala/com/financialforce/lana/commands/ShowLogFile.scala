edc99f62e082b2e2dd858f552fe543af195ea1bc
Kevin Jones <nawforce@gmail.com>
2020-12-12 13:12:47 +0000

    Handle no uri being passed to ShowLogFile comand

--- lana/src/main/scala/com/financialforce/lana/commands/ShowLogFile.scala
+++ lana/src/main/scala/com/financialforce/lana/commands/ShowLogFile.scala
@@ -4,13 +4,14 @@
 package com.financialforce.lana.commands
 
 import com.financialforce.lana.display.QuickPickWorkspace
-import com.financialforce.lana.runtime.vscode.{Uri, WebviewPanel}
+import com.financialforce.lana.runtime.vscode.{Uri, window}
 import com.financialforce.lana.{Context, Main}
 import io.scalajs.nodejs.fs.Fs
 import io.scalajs.nodejs.path.Path
 
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scala.scalajs.js
 
 object ShowLogFile extends LogView {
   def apply(context: Context): Unit = {
@@ -18,18 +19,28 @@ object ShowLogFile extends LogView {
     context.display.output(s"Registered command '${Main.appName}: Show Log'")
   }
 
-  private def command(context: Context, uri: Uri): Future[WebviewPanel] = {
-    val cmd =
-      QuickPickWorkspace.pickOrReturn(context) flatMap { wsPath =>
-        val filePath = uri.fsPath
-        val name = Path.parse(filePath).name.getOrElse("Unknown")
-        val fileContents = Fs.readFileSync(filePath, "utf-8")
-        Future.successful(createView(wsPath, context, name, filePath, fileContents))
-      }
-    cmd.failed foreach { ex =>
-      context.display.showErrorMessage(ex.getMessage)
+  private def command(context: Context, uri: Uri): Future[Unit] = {
+    val filePath = if (js.isUndefined(uri)) {
+      window.activeTextEditor.map(_.document.uri.fsPath).toOption
+    } else {
+      Some(uri.fsPath)
     }
-    cmd
-  }
 
+    filePath.map(filePath => {
+      val cmd =
+        QuickPickWorkspace.pickOrReturn(context) flatMap { wsPath =>
+          val name = Path.parse(filePath).name.getOrElse("Unknown")
+          val fileContents = Fs.readFileSync(filePath, "utf-8")
+          createView(wsPath, context, name, filePath, fileContents)
+          Future.successful(())
+        }
+      cmd.failed foreach { ex =>
+        context.display.showErrorMessage(ex.getMessage)
+      }
+      cmd
+    }).getOrElse {
+      context.display.showErrorMessage("No file selected to display log analysis")
+      Future.successful(())
+    }
+  }
 }
