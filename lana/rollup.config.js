69e135e48086c1f0a35b6d7090a264eea8ff4f77
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2021-10-08 17:41:42 +0100

    perf: switch typescript plugin from typescript2 to typescript

--- lana/rollup.config.js
+++ lana/rollup.config.js
@@ -1,15 +1,10 @@
 import { terser } from "rollup-plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 const compact = !process.env.ROLLUP_WATCH;
-const plugins = [
-  nodeResolve(),
-  commonjs(),
-  typescript(),
-  compact && terser()
-];
+const plugins = [nodeResolve(), commonjs(), typescript(), compact && terser()];
 
 export default {
   input: "src/Main.ts",
