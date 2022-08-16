c07c3b5a8c83385aeb5f4f8d82f6d248d08d23f3
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2022-08-16 14:29:21 +0100

    style: format all remaining files with prettier

--- log-viewer/rollup.config.js
+++ log-viewer/rollup.config.js
@@ -1,23 +1,23 @@
 // Rollup plugins
-import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const compact = !process.env.ROLLUP_WATCH;
 export default {
-  input: "modules/Main.ts",
+  input: 'modules/Main.ts',
   output: {
-    file: "out/bundle.js",
+    file: 'out/bundle.js',
     sourcemap: true,
   },
   plugins: [
     nodeResolve({ preferBuiltins: false }),
     commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
+    typescript({ tsconfig: './tsconfig.json' }),
     postcss({
-      extensions: [".css"],
+      extensions: ['.css'],
       minimize: true,
     }),
     compact && terser(),
