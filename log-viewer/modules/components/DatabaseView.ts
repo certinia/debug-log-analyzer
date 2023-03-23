4bc86a42000aaa0720cb257b8e1b7f36f162168c
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2023-03-23 15:42:18 +0000

    chore: remove @ts-expect-error comment
    The type defintion has been updated

--- log-viewer/modules/components/DatabaseView.ts
+++ log-viewer/modules/components/DatabaseView.ts
@@ -101,7 +101,6 @@ function renderDMLTable() {
         sorter: 'number',
         width: 110,
         bottomCalc: 'sum',
-        // @ts-expect-error: waiting for types defintion update https://github.com/DefinitelyTyped/DefinitelyTyped/pull/64309
         bottomCalcParams: { precision: 2 },
       },
     ],
@@ -303,7 +302,6 @@ function renderSOQLTable() {
         sorter: 'number',
         width: 110,
         bottomCalc: 'sum',
-        // @ts-expect-error: waiting for types defintion update https://github.com/DefinitelyTyped/DefinitelyTyped/pull/64309
         bottomCalcParams: { precision: 2 },
       },
       {
