fa8982016bf55ca057f482d96ae6af04332d3eea
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2023-04-18 18:20:08 +0100

    test: add database tests

--- log-viewer/modules/__tests__/Database.test.ts
+++ log-viewer/modules/__tests__/Database.test.ts
@@ -1,8 +1,28 @@
 /*
  * Copyright (c) 2020 FinancialForce.com, inc. All rights reserved.
  */
-// import { DatabaseAccess } from '../Database';
-// import parseLog from '../parsers/TreeParser';
-// import { getRootMethod } from '../parsers/TreeParser';
+import { DatabaseAccess } from '../Database';
+import parseLog, { getRootMethod } from '../parsers/TreeParser';
 
-describe('Analyse database tests', () => {});
+describe('Analyse database tests', () => {
+  it('Only DML and SOQL are collected', async () => {
+    const log =
+      '09:18:22.6 (6508409)|USER_INFO|[EXTERNAL]|0050W000006W3LM|jwilson@57dev.financialforce.com|Greenwich Mean Time|GMT+01:00\n' +
+      '09:18:22.6 (6574780)|EXECUTION_STARTED\n' +
+      '09:18:22.6 (6586704)|CODE_UNIT_STARTED|[EXTERNAL]|066d0000002m8ij|pse.VFRemote: pse.SenchaTCController invoke(saveTimecard)\n' +
+      '17:33:36.2 (1672655920)|SOQL_EXECUTE_BEGIN|[198]|Aggregations:0|SELECT Id FROM Account\n' +
+      '17:33:36.2 (1678684460)|SOQL_EXECUTE_END|[198]|Rows:3\n' +
+      '07:54:17.2 (1684126610)|DML_BEGIN|[774]|Op:Insert|Type:codaCompany__c|Rows:2\n' +
+      '09:19:13.82 (51592737891)|CODE_UNIT_FINISHED|pse.VFRemote: pse.SenchaTCController invoke(saveTimecard)\n' +
+      '09:19:13.82 (51595120059)|EXECUTION_FINISHED\n';
+
+    await parseLog(log);
+    const result = await DatabaseAccess.create(getRootMethod());
+    const firstSOQL = result.getSOQLLines()[0];
+
+    expect(firstSOQL.text).toEqual('SELECT Id FROM Account');
+
+    const firstDML = result.getDMLLines()[0];
+    expect(firstDML.text).toEqual('DML Op:Insert Type:codaCompany__c');
+  });
+});
