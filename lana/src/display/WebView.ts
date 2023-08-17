301be0021c5f7dbf55aae0ede617a7ae0878f35a
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2023-08-17 17:42:44 +0100

    refactor: remove export from class where not needed

--- lana/src/display/WebView.ts
+++ lana/src/display/WebView.ts
@@ -3,7 +3,13 @@
  */
 import { Uri, WebviewPanel, WebviewPanelOptions, window } from 'vscode';
 
-export class WebViewOptions implements WebviewPanelOptions {
+export class WebView {
+  static apply(name: string, title: string, resourceRoots: Uri[]): WebviewPanel {
+    return window.createWebviewPanel(name, title, -1, new WebViewOptions(resourceRoots));
+  }
+}
+
+class WebViewOptions implements WebviewPanelOptions {
   enableCommandUris = true;
   enableScripts = true;
   retainContextWhenHidden = true;
@@ -12,11 +18,5 @@ export class WebViewOptions implements WebviewPanelOptions {
 
   constructor(resourceRoots: Uri[]) {
     this.localResourceRoots = resourceRoots;
-  }
-}
-
-export class WebView {
-  static apply(name: string, title: string, resourceRoots: Uri[]): WebviewPanel {
-    return window.createWebviewPanel(name, title, -1, new WebViewOptions(resourceRoots));
   }
 }
