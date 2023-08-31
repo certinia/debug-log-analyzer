f4cd182b1e0cfb9abf645fabdc90b5d1aa9b2396
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2023-08-31 16:57:09 +0100

    feat: new app header
    - Help icon moves to the top right of the navbar
    - Tabs look more vscode like

--- log-viewer/modules/Main.ts
+++ log-viewer/modules/Main.ts
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2020 Certinia Inc. All rights reserved.
  */
+import { provideVSCodeDesignSystem, vsCodeButton } from '@vscode/webview-ui-toolkit';
+
 import '../resources/css/Settings.css';
 import '../resources/css/Status.css';
 import '../resources/css/Tabber.css';
@@ -19,6 +21,8 @@ import parseLog, {
 } from './parsers/TreeParser';
 import { hostService } from './services/VSCodeService';
 import renderTimeline, { renderTimelineKey, setColors } from './timeline/Timeline';
+
+provideVSCodeDesignSystem().register(vsCodeButton());
 
 let logSize: number;
 export let rootMethod: RootNode;
@@ -167,9 +171,17 @@ function onInit(): void {
   const tabHolder = document.querySelector('.tab-holder');
   tabHolder?.querySelectorAll('.tab').forEach((t) => t.addEventListener('click', onTabSelect));
 
-  const helpButton = document.querySelector('.help__link');
-  if (helpButton) {
-    helpButton.addEventListener('click', () => hostService().openHelp());
+  const rightNavBar = document.querySelector('.navbar--right');
+  if (rightNavBar) {
+    // TODO: refactor to a component + refactor app header bar too
+    // TODO: Ensure we have vscode Outgoing link protection for this link
+    // TODO: use @vscode/codicons instead of the svg
+    const helpButtonWrapper = document.createElement('div');
+    helpButtonWrapper.addEventListener('click', () => hostService().openHelp());
+    helpButtonWrapper.innerHTML = `<vscode-button appearance="icon" aria-label="Help" title="Help" class="help__icon">
+    <svg width="24" height="24" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.07505 4.10001C5.07505 2.91103 6.25727 1.92502 7.50005 1.92502C8.74283 1.92502 9.92505 2.91103 9.92505 4.10001C9.92505 5.19861 9.36782 5.71436 8.61854 6.37884L8.58757 6.4063C7.84481 7.06467 6.92505 7.87995 6.92505 9.5C6.92505 9.81757 7.18248 10.075 7.50005 10.075C7.81761 10.075 8.07505 9.81757 8.07505 9.5C8.07505 8.41517 8.62945 7.90623 9.38156 7.23925L9.40238 7.22079C10.1496 6.55829 11.075 5.73775 11.075 4.10001C11.075 2.12757 9.21869 0.775024 7.50005 0.775024C5.7814 0.775024 3.92505 2.12757 3.92505 4.10001C3.92505 4.41758 4.18249 4.67501 4.50005 4.67501C4.81761 4.67501 5.07505 4.41758 5.07505 4.10001ZM7.50005 13.3575C7.9833 13.3575 8.37505 12.9657 8.37505 12.4825C8.37505 11.9992 7.9833 11.6075 7.50005 11.6075C7.0168 11.6075 6.62505 11.9992 6.62505 12.4825C6.62505 12.9657 7.0168 13.3575 7.50005 13.3575Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+    </vscode-button>`;
+    rightNavBar.appendChild(helpButtonWrapper);
   }
 
   hostService().getConfig();
