61f354c4f8d2dd459203c16391a250aa4e4ed716
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2023-12-15 16:54:20 +0000

    perf: dynamically import apex-node + sfdx-auth-helper
    This will create chunks and allow for the js to be loaded and
    parsed faster as the chunks will be parsed later.

--- lana/src/salesforce/logs/GetLogFiles.ts
+++ lana/src/salesforce/logs/GetLogFiles.ts
@@ -1,15 +1,16 @@
 /*
  * Copyright (c) 2020 Certinia Inc. All rights reserved.
  */
-import { AuthHelper } from '@apexdevtools/sfdx-auth-helper';
-import { type LogRecord, LogService } from '@salesforce/apex-node';
+import { type LogRecord } from '@salesforce/apex-node';
 
 export class GetLogFiles {
   static async apply(wsPath: string): Promise<LogRecord[]> {
+    const { AuthHelper } = await import('@apexdevtools/sfdx-auth-helper');
     const ah = await AuthHelper.instance(wsPath);
     const connection = await ah.connect(await ah.getDefaultUsername());
 
     if (connection) {
+      const { LogService } = await import('@salesforce/apex-node');
       return new LogService(connection).getLogRecords();
     }
     return [];
