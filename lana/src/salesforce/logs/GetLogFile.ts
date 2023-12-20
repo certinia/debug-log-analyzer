63da9e4ff765c4af8eafd4964f9a8165f9d44289
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2023-12-20 11:18:10 +0000

    chore: disable eslint naming rule for dynamic imports

--- lana/src/salesforce/logs/GetLogFile.ts
+++ lana/src/salesforce/logs/GetLogFile.ts
@@ -4,11 +4,13 @@
 
 export class GetLogFile {
   static async apply(wsPath: string, logDir: string, logId: string): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     const { AuthHelper } = await import('@apexdevtools/sfdx-auth-helper');
     const ah = await AuthHelper.instance(wsPath);
     const connection = await ah.connect(await ah.getDefaultUsername());
 
     if (connection) {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
       const { LogService } = await import('@salesforce/apex-node');
       await new LogService(connection).getLogs({ logId: logId, outputDir: logDir });
     }
