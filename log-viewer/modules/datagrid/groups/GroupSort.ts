a2cdcc628ce4e965de2267f220944340754901fc
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2025-04-22 11:47:21 +0100

    fix: sorting of grouped rows

--- log-viewer/modules/datagrid/groups/GroupSort.ts
+++ log-viewer/modules/datagrid/groups/GroupSort.ts
@@ -12,11 +12,7 @@ export class GroupSort extends Module {
   initialize() {
     // @ts-expect-error groupSort is a custom propoerty see registerTableOption above
     if (this.table.options.groupSort) {
-      this.table.on('dataSorting', () => {
-        this.table.blockRedraw();
-        this._sortGroups();
-        this.table.restoreRedraw();
-      });
+      this.subscribe('sort-changed', this._sortGroups.bind(this));
     }
   }
 
@@ -47,41 +43,55 @@ export class GroupSort extends Module {
       ? this.table.options.groupBy
       : [this.table.options.groupBy];
     const { options } = this.table;
-    options.groupValues = [];
 
     const validGrps = grpArray.filter(Boolean).length > 0;
-    if (this.table && this.table.options.sortMode !== 'remote' && validGrps) {
+    if (this.table && options.sortMode !== 'remote' && validGrps) {
+      const originalGroupVals = options.groupValues ?? [[]];
       const { modules } = this.table;
 
-      const groupRows = modules.groupRows;
+      const groupsByKey: { [key: string]: unknown[] } = {};
       const rows = this.table.rowManager.rows;
-      groupRows.configureGroupSetup();
-      groupRows.generateGroups(rows);
+
+      let groupFunc = grpArray[0];
+      const grpField = groupFunc as string;
+      if (typeof groupFunc === 'string') {
+        groupFunc = function (data) {
+          return data[grpField];
+        };
+      }
+
+      if (groupFunc) {
+        rows.forEach((row: InternalColumnTotal) => {
+          const grpVal = groupFunc(row.data);
+          let groupRows = groupsByKey[grpVal];
+          if (!groupRows) {
+            groupRows = [];
+            groupsByKey[grpVal] = groupRows;
+          }
+          groupRows.push(row);
+        });
+      }
 
       const groupTotalsRows: InternalColumnTotal[] = [];
       const columnCalcs = modules.columnCalcs;
       const field = columnCalcs.botCalcs[0].field;
-      groupRows.groupList.forEach((group: { key: string; rows: { data: unknown }[] }) => {
-        const row = columnCalcs.generateBottomRow(group.rows);
-        row.data[field] = group.key;
-        row.key = group.key;
-        row.rows = group.rows;
+
+      for (const [key, rows] of Object.entries(groupsByKey)) {
+        const row = columnCalcs.generateBottomRow(rows);
+        row.data[field] = key;
+        row.key = key;
+        row.rows = rows;
         row.generateCells();
         groupTotalsRows.push(row);
-      });
-
+      }
       const sortListActual: unknown[] = [];
-      //build list of valid sorters and trigger column specific callbacks before sort begins
       const sorter = modules.sort;
       const sortList: InternalSortItem[] = options.sortOrderReverse
         ? sorter.sortList.slice().reverse()
         : sorter.sortList;
       sortList.forEach((item) => {
-        let sortObj;
-
         if (item.column) {
-          sortObj = item.column.modules.sort;
-
+          const sortObj = item.column.modules.sort;
           if (sortObj) {
             //if no sorter has been defined, take a guess
             if (!sortObj.sorter) {
@@ -115,7 +125,12 @@ export class GroupSort extends Module {
         groupValues.push(colTotals.data[field] as string);
       });
 
-      this.table?.setGroupValues([groupValues]);
+      const ogGroups = originalGroupVals[0] ?? [];
+      if (!this._areGroupsEqual(groupValues, ogGroups)) {
+        this.table?.setGroupValues([groupValues]);
+      }
+    } else {
+      this.table?.setGroupValues([]);
     }
   }
 }
