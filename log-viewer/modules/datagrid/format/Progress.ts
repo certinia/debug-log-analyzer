7fefeda5244a7d5b5b20bb7a46f702eb415eb1b6
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2025-05-25 15:36:12 +0100

    perf: avoid adding percent bar to dom when % is 0
    Smaller dom size

--- log-viewer/modules/datagrid/format/Progress.ts
+++ log-viewer/modules/datagrid/format/Progress.ts
@@ -7,28 +7,29 @@ import './Progress.css';
 
 export function progressFormatter(
   cell: CellComponent,
-  formatterParams: Params,
+  formatterParams: ProgressParams,
   _onRendered: EmptyCallback,
 ) {
   const value = (cell.getValue() || 0) / 1000000;
-
   const roundedValue = `${value.toFixed(formatterParams.precision || 3)}`;
 
   if (formatterParams.totalValue !== undefined && formatterParams.totalValue !== null) {
+    const totalValAsMs =
+      formatterParams.totalValue !== 0 ? formatterParams.totalValue / 1000000 : 0;
     const percentComplete =
-      formatterParams.totalValue !== 0 ? (value / (formatterParams.totalValue / 1000000)) * 100 : 0;
-    const percentage = `(${percentComplete.toFixed(2)}%)`;
+      totalValAsMs !== 0 ? (Math.round((value / totalValAsMs) * 100) / 100) * 100 : 0;
+    const percentageText = `(${percentComplete.toFixed(2)}%)`;
 
     return `<div class="progress-wrapper">
-        <div class="progress-bar" style="width: ${percentComplete}%;"></div>
-        <div class="progress-bar__text"><span>${roundedValue}</span><span class="progress-bar__text__percent">${percentage}</span></div>
+        ${percentComplete ? `<div class="progress-bar" style="width: ${percentComplete}%;"></div>` : ''}
+        <div class="progress-bar__text"><span>${roundedValue}</span><span class="progress-bar__text__percent">${percentageText}</span></div>
       </div>`;
   }
 
   return `${roundedValue}`;
 }
 
-export interface Params {
+export interface ProgressParams {
   precision?: number;
   totalValue?: number;
 }
