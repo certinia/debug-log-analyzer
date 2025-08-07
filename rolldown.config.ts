0f2d4debaa91887f89e8e374d9fe1f1f006a6b32
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2025-08-07 15:02:05 +0100

    chore: reusable swcOptions function

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -6,6 +6,24 @@ import nodePolyfills from 'rollup-plugin-polyfill-node';
 import postcss from 'rollup-plugin-postcss';
 import { defineRollupSwcOption, swc } from 'rollup-plugin-swc3';
 
+const getSwcOptions = (dirPath: string) =>
+  defineRollupSwcOption({
+    include: /\.[mc]?[jt]sx?$/,
+    exclude: 'node_modules',
+    tsconfig: production ? `${dirPath}/tsconfig.json` : `${dirPath}/tsconfig-dev.json`,
+    jsc: {
+      transform: { useDefineForClassFields: false },
+      minify: {
+        compress: production,
+        mangle: production
+          ? {
+              keepClassNames: true,
+            }
+          : false,
+      },
+    },
+  });
+
 const production = process.env.NODE_ENV === 'production';
 console.log('Package mode:', production ? 'production' : 'development');
 export default defineConfig([
@@ -20,28 +38,10 @@ export default defineConfig([
     platform: 'node',
     external: ['vscode'],
     resolve: { tsconfigFilename: production ? './lana/tsconfig.json' : './lana/tsconfig-dev.json' },
-    plugins: [
-      swc(
-        defineRollupSwcOption({
-          include: /\.[mc]?[jt]sx?$/,
-          exclude: 'node_modules',
-          tsconfig: production ? './lana/tsconfig.json' : './lana/tsconfig-dev.json',
-          jsc: {
-            minify: {
-              compress: production,
-              mangle: production
-                ? {
-                    keep_classnames: true,
-                  }
-                : false,
-            },
-          },
-        }),
-      ),
-    ],
+    plugins: [swc(getSwcOptions('./lana'))],
   },
   {
-    input: { bundle: './log-viewer/modules/Main.ts' },
+    input: { bundle: './log-viewer/src/Main.ts' },
     output: [
       {
         format: 'esm',
@@ -64,25 +64,7 @@ export default defineConfig([
         extensions: ['.css', '.scss'],
         minimize: true,
       }),
-      swc(
-        defineRollupSwcOption({
-          // All options are optional
-          include: /\.[mc]?[jt]sx?$/,
-          exclude: 'node_modules',
-          tsconfig: production ? './log-viewer/tsconfig.json' : './log-viewer/tsconfig-dev.json',
-          jsc: {
-            transform: { useDefineForClassFields: false },
-            minify: {
-              compress: production,
-              mangle: production
-                ? {
-                    keep_classnames: true,
-                  }
-                : false,
-            },
-          },
-        }),
-      ),
+      swc(getSwcOptions('./log-viewer')),
       copy({
         hook: 'closeBundle',
         targets: [
