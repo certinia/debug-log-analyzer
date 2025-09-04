6f4047ebdda28c5a85b6bfee335487a73093a2ba
Josh Coulter <joshcoulteruk@gmail.com>
2025-09-04 10:57:44 +0100

    refactor: reduce jest config duplication

--- jest.config.js
+++ jest.config.js
@@ -1,57 +1,46 @@
+/**
+ * @param {string} displayName
+ * @param {string} rootDir
+ */
+const defineProject = (displayName, rootDir) => ({
+  displayName,
+  rootDir,
+  testEnvironment: 'node',
+
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.(ts|js)?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'esnext',
+          parser: { decorators: true, syntax: 'typescript' },
+        },
+      },
+    ],
+  },
+  transformIgnorePatterns: [
+    // allow lit/@lit transformation
+    '<rootDir>/node_modules/(?!@?lit)',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/out/'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+});
+
+const projectConfigs = [
+  {
+    displayName: 'log-viewer',
+    rootDir: '<rootDir>/log-viewer',
+  },
+  {
+    displayName: 'lana',
+    rootDir: '<rootDir>/lana',
+  },
+];
+
 /** @type {import('@jest/types').Config.InitialOptions} */
 export default {
-  projects: [
-    {
-      displayName: 'log-viewer',
-      rootDir: '<rootDir>/log-viewer',
-      testEnvironment: 'node',
-
-      moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-      },
-      transform: {
-        '^.+\\.(ts|js)?$': [
-          '@swc/jest',
-          {
-            jsc: {
-              target: 'esnext',
-              parser: { decorators: true, syntax: 'typescript' },
-            },
-          },
-        ],
-      },
-      transformIgnorePatterns: [
-        // allow lit/@lit transformation
-        '<rootDir>/node_modules/(?!@?lit)',
-      ],
-      testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/out/'],
-      extensionsToTreatAsEsm: ['.ts', '.tsx'],
-    },
-    {
-      displayName: 'lana',
-      rootDir: '<rootDir>/lana',
-      testEnvironment: 'node',
-
-      moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-      },
-      transform: {
-        '^.+\\.(ts|js)?$': [
-          '@swc/jest',
-          {
-            jsc: {
-              target: 'esnext',
-              parser: { decorators: true, syntax: 'typescript' },
-            },
-          },
-        ],
-      },
-      transformIgnorePatterns: [
-        // allow lit/@lit transformation
-        '<rootDir>/node_modules/(?!@?lit)',
-      ],
-      testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/out/'],
-      extensionsToTreatAsEsm: ['.ts', '.tsx'],
-    },
-  ],
+  projects: projectConfigs.map(({ displayName, rootDir }) => defineProject(displayName, rootDir)),
 };
