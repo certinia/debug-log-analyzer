36c08f15ef8d73826bbc4a4b838d64ea7c173547
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2025-12-01 10:29:50 +0000

    feat: add IconButton component with badge support

---
+++ log-viewer/src/components/IconButton.ts
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2025 Certinia Inc. All rights reserved.
+ */
+import { provideVSCodeDesignSystem, vsCodeBadge, vsCodeButton } from '@vscode/webview-ui-toolkit';
+import { LitElement, css, html, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+// styles
+import codiconStyles from '../styles/codicon.css';
+import { globalStyles } from '../styles/global.styles.js';
+
+provideVSCodeDesignSystem().register(vsCodeButton(), vsCodeBadge());
+
+@customElement('icon-button')
+export class IconButton extends LitElement {
+  static styles = [
+    globalStyles,
+    unsafeCSS(codiconStyles),
+    css`
+      vscode-button {
+        height: 22px;
+        width: 22px;
+      }
+
+      .badge-indicator::part(control) {
+        --design-unit: 0;
+        --border-width: 0;
+
+        color: rgb(255, 255, 255);
+        background-color: rgb(0, 120, 212);
+        position: absolute;
+        top: 10px;
+        right: 0;
+        font-size: 9px;
+        font-weight: 600;
+        min-width: 12px;
+        height: 12px;
+        line-height: 12px;
+        padding: 0 2px;
+        border-radius: 16px;
+        text-align: center;
+        display: inline-block;
+        box-sizing: border-box;
+      }
+    `,
+  ];
+
+  @property()
+  icon: string = '';
+
+  @property()
+  badgeCount: number | null | undefined = null;
+
+  @property()
+  ariaLabel: string = 'Icon Button';
+
+  @property()
+  title: string = 'Icon Button';
+
+  render() {
+    console.log('Rendering IconButton with badgeCount:', this.badgeCount);
+    const indicator =
+      this.badgeCount !== null && this.badgeCount !== undefined
+        ? html`<vscode-badge class="badge-indicator">${this.badgeCount}</vscode-badge> `
+        : ``;
+
+    return html`<div class="menu-container">
+      <vscode-button appearance="icon" aria-label="${this.ariaLabel}" title="${this.title}">
+        <span class="codicon ${this.icon}"></span>
+        ${indicator}
+      </vscode-button>
+    </div>`;
+  }
+}
