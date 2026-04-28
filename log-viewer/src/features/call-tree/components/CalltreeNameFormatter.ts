8d14f8af89d6bbc15361397ef313860ed9dd1cf8
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2026-04-28 12:29:48 +0100

    refactor: calltreeNameFormatter switch to dataTree?.index instead of treeLevel

--- log-viewer/src/features/call-tree/components/CalltreeNameFormatter.ts
+++ log-viewer/src/features/call-tree/components/CalltreeNameFormatter.ts
@@ -12,16 +12,18 @@ export function createCalltreeNameFormatter(excludedTypes: Set<LogEventType>) {
     _formatterParams: object,
     _onRendered: EmptyCallback,
   ): string | HTMLElement {
-    const data = cell.getData() as { originalData: LogEvent; treeLevel: number };
-    const { originalData: node, treeLevel } = data;
     // @ts-expect-error this.table is added by tabulator when the formatter is called, but isn't in the types for some reason
     childIndent ??= this.table.options.dataTreeChildIndent ?? 9;
+    const row = cell.getRow();
+    // @ts-expect-error _row is private to tabulator but is the only way to get the tree level in a formatter
+    const treeLevel: number = row._row.modules.dataTree?.index ?? 0;
     const levelIndent = treeLevel * childIndent;
 
     const cellElem = cell.getElement();
     cellElem.style.paddingLeft = `${levelIndent + 4}px`;
     cellElem.style.textIndent = `-${levelIndent}px`;
 
+    const { originalData: node } = cell.getData() as { originalData: LogEvent };
     if (node.hasValidSymbols) {
       const link = document.createElement('a');
       link.setAttribute('href', '#!');
