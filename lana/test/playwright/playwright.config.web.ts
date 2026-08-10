d002f6f687b800047550b56e51ba10908d04033c
peternhale <peter.hale@salesforce.com>
2026-08-10 10:40:55 -0600

    ci: rework release automation

--- lana/test/playwright/playwright.config.web.ts
+++ lana/test/playwright/playwright.config.web.ts
@@ -3,6 +3,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   ...createWebConfig({ testDir: './specs', workers: 1, fullyParallel: false }),
+  outputDir: '../../test-results/web',
+  reporter: [['html', { open: 'never', outputFolder: '../../playwright-report/web' }]],
   testMatch: '**/*.web.spec.ts',
 };
 
