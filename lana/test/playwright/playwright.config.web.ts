b03c4f1e7fb01dfd910ea4853730951bfc4b69f6
peternhale <peter.hale@salesforce.com>
2026-08-21 11:10:49 -0600

    test(lana): enable WebGL in headless CI

--- lana/test/playwright/playwright.config.web.ts
+++ lana/test/playwright/playwright.config.web.ts
@@ -1,8 +1,22 @@
 import { createWebConfig } from '@salesforce/playwright-vscode-ext';
 import type { PlaywrightTestConfig } from '@playwright/test';
 
+const baseConfig = createWebConfig({ testDir: './specs', workers: 1, fullyParallel: false });
+
 const config: PlaywrightTestConfig = {
-  ...createWebConfig({ testDir: './specs', workers: 1, fullyParallel: false }),
+  ...baseConfig,
+  use: {
+    ...baseConfig.use,
+    launchOptions: {
+      ...baseConfig.use?.launchOptions,
+      args: [
+        ...(baseConfig.use?.launchOptions?.args ?? []),
+        ...(process.env.CI
+          ? ['--use-gl=angle', '--use-angle=swiftshader-webgl', '--enable-unsafe-swiftshader']
+          : []),
+      ],
+    },
+  },
   outputDir: '../../test-results/web',
   reporter: [['html', { open: 'never', outputFolder: '../../playwright-report/web' }]],
   testMatch: '**/*.web.spec.ts',
